80a9c5a714bafff1c7727dca699167dba734f7e4
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-05-31 17:52:00 +0300

    added state class

--- src/main/java/DFA/State.java
+++ src/main/java/DFA/State.java
@@ -1,4 +1,33 @@
 package DFA;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class State {
+
+    private String id;
+    private boolean finite;
+    private Map<Character, State> transition;
+
+    public State(String id, boolean finite, Map<Character, State> transition) {
+        this.id = id;
+        this.finite = finite;
+        this.transition = new HashMap<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isFinite() {
+        return finite;
+    }
+
+    public void addTransition(Character symbol, State nextState) {
+        this.transition.put(symbol, nextState);
+    }
+
+    public State getTransition(Character symbol) {
+        return transition.get(symbol);
+    }
 }
