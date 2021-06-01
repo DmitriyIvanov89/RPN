06ccbaf414445693f7a1f86635735d964458c9ba
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-06-01 14:04:30 +0300

    mod main method

--- src/main/java/DFA/DFAConfigReader.java
+++ src/main/java/DFA/DFAConfigReader.java
@@ -2,9 +2,7 @@ package DFA;
 
 import com.google.gson.Gson;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class DFAConfigReader {
 
