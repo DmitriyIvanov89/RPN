00e8ae945aa046fa2ff18f04f4973db1e684f844
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-06-01 18:13:19 +0300

    mod Token class & token type

--- src/main/java/LexemeAnalyze/Token.java
+++ src/main/java/LexemeAnalyze/Token.java
@@ -1,4 +1,20 @@
 package LexemeAnalyze;
 
 public class Token {
+
+    private String value;
+    private TokenType type;
+
+    public Token(String value, TokenType type) {
+        this.value = value;
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
 }
