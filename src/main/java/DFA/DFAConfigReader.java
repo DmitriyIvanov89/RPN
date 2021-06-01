48f466f5d8377229c9d7f7a368363f13779a40af
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-06-01 12:45:45 +0300

    mod DFAReader

--- src/main/java/DFA/DFAConfigReader.java
+++ src/main/java/DFA/DFAConfigReader.java
@@ -1,4 +1,22 @@
 package DFA;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DFAConfigReader {
+
+    private String configPath;
+
+    public DFAConfigReader(String configPath) {
+        this.configPath = configPath;
+    }
+
+    public DFAConfig readConfigJson(String configPath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(configPath))) {
+            return new Gson().fromJson(reader, DFAConfig.class);
+        }
+    }
 }
