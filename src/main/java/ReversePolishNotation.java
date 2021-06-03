89069d178065cbcc7c6e39f72c5026ebc61783ff
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-06-03 17:30:14 +0300

    some changes

--- src/main/java/ReversePolishNotation.java
+++ src/main/java/ReversePolishNotation.java
@@ -1,19 +1,22 @@
 import DFA.DFA;
 import DFA.DFAConfig;
 import DFA.DFAConfigReader;
+import lexicalAnalyzer.Lexer;
 
 import java.io.IOException;
 
 public class ReversePolishNotation {
 
-    private final static String CONFIG_PATH = "d:\\my\\RPE_repo\\src\\main\\resources\\dfaConfig.json";
+    //private final static String CONFIG_PATH = "d:\\my\\RPE_repo\\src\\main\\resources\\dfaConfig.json";
 
     public static void main(String[] args) throws IOException {
 
-        DFAConfigReader reader = new DFAConfigReader(CONFIG_PATH);
-        DFAConfig config = reader.readConfigJson();
+//        DFAConfigReader reader = new DFAConfigReader(CONFIG_PATH);
+//        DFAConfig config = reader.readConfigJson();
+//        DFA dfa = new DFA(config);
 
-        DFA dfa = new DFA(config);
-
+        String expression = "22+36";
+        Lexer lexer = new Lexer(expression);
+        lexer.getNextToken();
     }
 }
--- src/main/java/lexicalAnalyzer/Lexer.java
+++ src/main/java/lexicalAnalyzer/Lexer.java
@@ -1,19 +1,43 @@
 package lexicalAnalyzer;
 
+import DFA.DFA;
+import DFA.DFAConfigReader;
+import DFA.DFAConfig;
+import DFA.State;
+
+import java.io.IOException;
+
 public class Lexer {
 
+    private final static String CONFIG_PATH = "d:\\my\\RPE_repo\\src\\main\\resources\\dfaConfig.json";
     private final String expr;
+    private final State startState;
 
-    public Lexer(String expr) {
+    public Lexer(String expr) throws IOException {
         this.expr = expr;
+        DFAConfigReader reader = new DFAConfigReader(CONFIG_PATH);
+        DFAConfig config = reader.readConfigJson();
+        DFA dfaLexer = new DFA(config);
+        this.startState = dfaLexer.getStartState();
     }
 
     public Token getNextToken() {
+        State currState = startState;
+        String value = "";
+        for (int i = 0; i < expr.length(); i++) {
+            currState = currState.getTransition(expr.charAt(i));
+            value += expr.charAt(i);
+            if (currState == null && currState.isFinite()) {
+                currState = startState;
+                return new Token(value, TokenType.NUMBER);
+            } else {
+                System.out.println("Unexpected token");
+            }
+        }
         return null;
     }
 
     public Token lookAHead() {
         return null;
     }
-
 }
