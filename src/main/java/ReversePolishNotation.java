31f09f02ebcb60f59c0236988de60a33f08a5ddc
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-06-08 23:43:33 +0300

    after merge

--- src/main/java/ReversePolishNotation.java
+++ src/main/java/ReversePolishNotation.java
@@ -6,8 +6,8 @@ public class ReversePolishNotation {
 
     public static void main(String[] args) throws IOException {
 
-        String input = "22+2";
-        Lexer lexer = new Lexer(input);
+        String expression = "22+2";
+        Lexer lexer = new Lexer(expression);
         System.out.println(lexer.getNextToken());
 
     }
--- src/main/java/lexicalAnalyzer/Lexer.java
+++ src/main/java/lexicalAnalyzer/Lexer.java
@@ -6,8 +6,9 @@ import java.io.IOException;
 
 public class Lexer {
 
-    private final static String CONFIG_PATH = "d:\\my\\RPE_repo\\src\\main\\resources\\dfaConfig.json";
-    //private final static String CONFIG_PATH = "e:\\IT\\JAVA\\my_project\\rpn_repo\\RPE\\src\\main\\resources\\dfaConfig.json";
+    //private final static String CONFIG_PATH = "d:\\my\\RPE_repo\\src\\main\\resources\\dfaConfig.json";
+    private final static String CONFIG_PATH = "e:\\IT\\JAVA\\my_project\\rpn_repo\\RPN\\src\\main\\resources\\dfaConfig.json";
+    private final String expr;
     private DFA dfa;
     private int position;
     private State currState;
