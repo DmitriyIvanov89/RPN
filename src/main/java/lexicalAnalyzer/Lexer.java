fa813bb25f7868094f0a160299ce8d2039572562
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-06-11 23:14:58 +0300

    some

---
+++ src/main/java/lexicalAnalyzer/Lexer.java
@@ -0,0 +1,23 @@
+package lexicalAnalyzer;
+
+import DFA.DFA;
+import DFA.DFAConfig;
+import DFA.DFAConfigReader;
+
+public class Lexer {
+
+    private final String expr;
+
+    public Lexer(String expr) {
+        this.expr = expr;
+    }
+
+    public Token getNextToken() {
+        return null;
+    }
+
+    public Token lookAHead() {
+        return null;
+    }
+
+}
