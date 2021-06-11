edc8e6e77725a1b759b5b6cd288624fc130c8c0a
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-06-11 23:24:43 +0300

    deleted conflicts after merge

--- src/main/java/ReversePolishNotation.java
+++ src/main/java/ReversePolishNotation.java
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-import lexicalAnalyzer.Lexer;
-=======
 import dfa.DFA;
 import dfa.DFAConfig;
 import dfa.DFAConfigReader;
 import lexer.Lexer;
 import lexer.TokenType;
->>>>>>> newDev
 
 import java.io.IOException;
 
@@ -17,16 +13,10 @@ public class ReversePolishNotation {
 
     public static void main(String[] args) throws IOException {
 
-<<<<<<< HEAD
-        String expression = "22+2";
-        Lexer lexer = new Lexer(expression);
-        System.out.println(lexer.getNextToken());
-=======
         DFAConfigReader reader = new DFAConfigReader(CONFIG_PATH);
         DFAConfig config = reader.readConfigJson();
         DFA dfa = new DFA(config);
         String input = "1.5*0.23-55/101";
->>>>>>> newDev
 
         Lexer lexer = new Lexer(input, dfa);
         while (lexer.lookAhead().getType() != TokenType.UNDEFINED && lexer.lookAhead().getType() != TokenType.EOF) {
--- src/main/java/lexer/TokenType.java
+++ src/main/java/lexer/TokenType.java
@@ -5,12 +5,7 @@ public enum TokenType {
     DEFAULT,
     NUMBER,
     OP_PLUS, OP_MINUS, OP_MUL, OP_DIV,
-<<<<<<< HEAD:src/main/java/lexicalAnalyzer/TokenType.java
-    DOT
-=======
     DOT,
     UNDEFINED,
     EOF
-
->>>>>>> newDev:src/main/java/lexer/TokenType.java
 }
