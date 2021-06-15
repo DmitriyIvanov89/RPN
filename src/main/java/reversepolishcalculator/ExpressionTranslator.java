238eac53aefa9ace2ee932c120ace810c11f8dc5
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-06-15 16:26:31 +0300

    added method in reverse class

--- src/main/java/reversepolishcalculator/ExpressionTranslator.java
+++ src/main/java/reversepolishcalculator/ExpressionTranslator.java
@@ -1,7 +1,23 @@
 package reversepolishcalculator;
 
+import lexer.Lexer;
+import lexer.Token;
+
+import java.util.List;
+import java.util.Stack;
+
 public class ExpressionTranslator {
 
-    
+    private final Lexer lexer;
+    private List<Token> tokens;
+    private Stack<Token> stackOperation;
+
+    public ExpressionTranslator(Lexer lexer) {
+        this.lexer = lexer;
+    }
+
+    public List<Token> translateExpression() {
+        return null;
+    }
 
 }
