4684c009688cec8abb146a3e2fdb8b8353eced00
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-06-24 17:50:31 +0300

    added test class fot TraversalResult class

---
+++ src/test/java/dfa/TraversalResultTest.java
@@ -0,0 +1,18 @@
+package dfa;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TraversalResultTest {
+
+
+
+    @Test
+    public void getState() {
+    }
+
+    @Test
+    public void getTrace() {
+    }
+}
