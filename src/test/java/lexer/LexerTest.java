ae38b081199110aab9ebc55afcb0539d26525b82
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-06-25 16:15:53 +0300

    mod test class Lexer

--- src/test/java/lexer/LexerTest.java
+++ src/test/java/lexer/LexerTest.java
@@ -1,9 +1,22 @@
 package lexer;
 
+import dfa.DFA;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.text.CharacterIterator;
 
 
 public class LexerTest {
+
+    private CharacterIterator iterator;
+    private DFA dfa;
+    private Lexer lexer;
+
+    @Before
+    public void beforeEach() {
+        
+    }
 
     @Test
     public void lookAhead() {
