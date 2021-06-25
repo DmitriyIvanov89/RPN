28bc4079ee15e89e68e6e192964af288c13daec8
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-06-25 15:56:02 +0300

    mod test startState in DFA class

--- src/main/java/dfa/TraversalResult.java
+++ src/main/java/dfa/TraversalResult.java
@@ -1,5 +1,7 @@
 package dfa;
 
+import java.util.Objects;
+
 public class TraversalResult {
 
     private final State state;
@@ -16,5 +18,18 @@ public class TraversalResult {
 
     public String getTrace() {
         return trace;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(state, trace);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        TraversalResult traversalResult = (TraversalResult) obj;
+        return state == traversalResult.state && Objects.equals(trace, traversalResult.trace);
     }
 }
--- src/test/java/dfa/DFATest.java
+++ src/test/java/dfa/DFATest.java
@@ -5,20 +5,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 
 public class DFATest {
 
     private State startState;
-    private DFAConfig dfaConfig;
-    private DFAConfigReader reader;
     private DFA dfa;
 
     private final static String CONFIG_PATH = "e:\\IT\\JAVA\\my_project\\rpn_repo\\RPN\\src\\main\\resources\\dfaConfig.json";
 
     @Before
     public void before() throws IOException {
-        reader = new DFAConfigReader(CONFIG_PATH);
-        dfaConfig = reader.readConfigJson();
+        DFAConfigReader reader = new DFAConfigReader(CONFIG_PATH);
+        DFAConfig dfaConfig = reader.readConfigJson();
         dfa = new DFA(dfaConfig);
         startState = new State("S0", false);
         startState.setName("DEFAULT");
@@ -31,9 +31,18 @@ public class DFATest {
 
     @Test
     public void testCheckString() {
+        State currState = startState;
+        String expression = "2 + 2";
+        Assert.assertTrue(dfa.checkString(expression));
     }
 
-    @Test
+    @Test(expected = StackOverflowError.class)
     public void testGreedyTraversal() {
+        State state = new State("S1", true);
+        String trace = "2";
+        TraversalResult traversalResult = new TraversalResult(state, trace);
+        String expression = "2+2";
+        CharacterIterator iterator = new StringCharacterIterator(expression);
+        Assert.assertEquals(traversalResult, dfa.greedyTraversal(iterator));
     }
 }
