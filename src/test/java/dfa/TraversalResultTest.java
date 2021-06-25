cf348affd016cb6309890a4166b9d6ad6b1d0071
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-06-25 10:34:04 +0300

    mod tests for TraversalResult class

--- src/test/java/dfa/TraversalResultTest.java
+++ src/test/java/dfa/TraversalResultTest.java
@@ -1,18 +1,30 @@
 package dfa;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 public class TraversalResultTest {
 
+    private TraversalResult testTraversalResult;
+    private State testState;
+    private String testTrace;
 
+    @Before
+    public void beforeEach() {
+        testState = new State("S0", false);
+        testTrace = "0";
+        testTraversalResult = new TraversalResult(testState, testTrace);
+    }
 
     @Test
     public void getState() {
+        Assert.assertEquals(testState, testTraversalResult.getState());
     }
 
     @Test
     public void getTrace() {
+        Assert.assertEquals(testTrace, testTraversalResult.getTrace());
     }
 }
